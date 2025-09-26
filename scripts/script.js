306c2e93c55aba4c8d7ab6ce2d832d4080f16e1a
vishnu-11124 <vishnusanthosh11124@gmail.com>
2025-09-26 13:33:24 +0530

    Added dark toggle, Modified overall ui

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 // splash screen 
 window.addEventListener("load", () => {
       const splash = document.querySelector(".splash");
-      const content = document.querySelector(".content");
+      const content = document.querySelector("nav");
 
       // Keep splash for 2.5s then hide
       setTimeout(() => {
@@ -20,3 +20,23 @@ function openMenu(){
 function closeMenu(){
     sideMenu.style.right = "-300px";
 }
+
+window.addEventListener("scroll", function(){
+    const nav = document.querySelector("nav");
+    const navLinks = document.querySelectorAll("nav ul");
+    if(this.scrollY > 50){
+        nav.classList.add("bg-white", "bg-opacity-50",'backdrop-blur-lg', 'shadow-sm');
+        navLinks.classList.remove('bg-white', 'bg-opacity-50','shadow-sm');
+    }
+    else{
+        nav.classList.remove("bg-white", "bg-opacity-50",'backdrop-blur-lg', 'shadow-sm');
+        navLinks.classList.add('bg-white', 'bg-opacity-50','shadow-sm');
+    }
+});
+
+// Dark and Light mode toggle
+const modeToggle = document.querySelector('.mode');
+modeToggle.addEventListener('click', () => {
+    document.body.classList.toggle('dark');
+    modeToggle.innerHTML = document.body.classList.contains('dark') ? '<img src="./images/sun_icon.png" class="w-6" alt="">' : '<img src="./images/moon_icon.png" class="w-6" alt="">';
+});
